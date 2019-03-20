d2c99bed1844de9b3b163570c95887494809cabf
Alexander Hurst <alexander@ahmail.ca>
2019-03-20 17:46:30 -0400

    added secondary crossover and mutation methods

--- main.py
+++ main.py
@@ -15,14 +15,30 @@ import salesman_graphics
 cityList = []
 
 # create cities
-for i in range(0, 60):
+for i in range(0, 15):
     cityList.append(salesman_objects.City(x=int(random.random() * 200),
                                           y=int(random.random() * 200)))
 
 # pprint(cityList)
 # get best route between them
-gen = salesman_genetics.GeneticAlgorithm(cityList, 100, 20, 0.01)
-gen.run(900)
+gen = salesman_genetics.GeneticAlgorithm(cityList, 120, 30, 0.01, original_crossover_method=True, original_mutation_method=True)
+gen2 = salesman_genetics.GeneticAlgorithm(cityList, 120, 30, 0.01, original_crossover_method=False, original_mutation_method=True)
+gen3 = salesman_genetics.GeneticAlgorithm(cityList, 120, 30, 0.01, original_crossover_method=True, original_mutation_method=False)
+gen.run(400)
+gen2.run(400)
+gen3.run(400)
+
+for i, best_one, best_two, best_three in zip(range(len(gen.gen_bests)), gen.gen_bests, gen2.gen_bests, gen3.gen_bests):
+    print("generation", i)
+    print("Original crossover and mutation")
+    pprint(best_one)
+    print()
+    print("Modified crossover")
+    pprint(best_two)
+    print()
+    print("Modified mutation")
+    pprint(best_three)
+    print()
 
 salesman_graphics.Plot_gen(gen.gen_bests)
 
--- salesman_genetics.py
+++ salesman_genetics.py
@@ -9,11 +9,13 @@ class GeneticAlgorithm:
     # best_size = the number of offspring to keep when doing a crossover
     
     # The genetic algorithm is run multiple times to find the best path with variously mutated populations
-    def __init__(self, city_list, pop_size, best_size, mutation_rate):
+    def __init__(self, city_list, pop_size, best_size, mutation_rate, original_crossover_method=True, original_mutation_method=True):
         self.pop_size = pop_size                # Initialize population size
         self.best_size = best_size              # Initialize the best size for calculations
         self.mutation_rate = mutation_rate      # Initialize the mutation rate for the mutation stage
         self.city_list = city_list              # Get the list of cities salesman must visit
+        self.crossover_method = original_crossover_method
+        self.mutation_method = original_mutation_method
 
         self.gen_bests = []
 
@@ -56,9 +58,18 @@ class GeneticAlgorithm:
             mating_pool.append(self.pop[i])
         return mating_pool
 
+
     # Breed the population
-    # 4 - CROSSOVER
+    # 4 - Crossover method selection
     def _crossover(self, mating_pool):
+        if(self.crossover_method):
+            return self._crossover1(mating_pool)
+        else:
+            return self._crossover2(mating_pool)
+
+    # Breed the population
+    # 4 - CROSSOVER, VERSION 1
+    def _crossover1(self, mating_pool):
         num_children = self.pop_size - self.best_size - 1
         children = []
 
@@ -85,9 +96,56 @@ class GeneticAlgorithm:
                      
         return children
 
+    # 4 - CROSSOVER, VERSION 2
+        # breeds three parents to make children
+        # parent 0 usually has the most influence
+        # parent 1 usually has the least influence
+        # parent 2 usually has the middle influence
+        # influence depending on city order and gene start + end points
+    def _crossover2(self, mating_pool):
+        num_children = self.pop_size - self.best_size - 1
+        children = []
+
+        for i in range (num_children):
+            parents = random.sample(mating_pool, 3)
+
+            child = []
+
+            # Create genes for the child to emerge from
+            geneA = int(random.random() * len(parents[0].route))
+            geneB = int(random.random() * len(parents[1].route))
+            geneC = int(random.random() * len(parents[2].route))
+
+            genes = sorted([geneA, geneB, geneC])
+
+            # Add the parent genes to the child
+            for i in range(genes[0], genes[1]):
+                child.append(parents[0].route[i])
+
+            for i in range(genes[1], genes[2]):
+                if parents[1].route[i] not in child:
+                    child.append(parents[1].route[i])
+
+            child.extend(item for item in parents[2].route if item not in child)
+
+            # Set a route for the child and add to list
+            children.append(Route(child))
+                    
+        return children
+    
+    # Mutate the population
+    # 5 Mutation selection
+
+    def _mutation(self, children):
+        if(self.mutation_method):
+            return self._mutation1(children)
+        else:
+            return self._mutation2(children)
+
+
     # Mutate the population
     # 5 - MUTATE, to defy local convergence
-    def _mutation(self, children): 
+    def _mutation1(self, children): 
         mutants = []
 
         for child in children:
@@ -98,10 +156,25 @@ class GeneticAlgorithm:
             mutants.append(child)
         return mutants
 
+    # Mutate the population
+    # allows multiple mutations to take place per city per child
+    # this shouldnt change much but does allow the possibility for more mutations
+    # 5 - MUTATE, to defy local convergence
+    def _mutation2(self, children): 
+        mutants = []
+
+        for child in children:
+            for i in range(len(self.city_list)):
+                while (random.random() < self.mutation_rate):
+                    a = random.random()
+                    child.route[int(a * len(self.city_list))], child.route[int(i)] = child.route[int(i)], child.route[int(a * len(self.city_list))]
+            mutants.append(child)
+        return mutants
+
     # 6 - REPEAT
     def run(self, number_of_iterations):        # Run the algorithm numerous times to get best results from mutated population
         for i in range(number_of_iterations):
-            self.gen_bests.append(self.pop[0].route)
+            self.gen_bests.append([self.pop[0].route, self.pop[0].fitness])
             parents = self._selection()         # set the parent population     
             children = self._crossover(parents) # Breed the population
             # print("creating mutants")
@@ -109,7 +182,7 @@ class GeneticAlgorithm:
             self.pop = parents
             self.pop.extend(mutants)            # Set the population to the mutated population
             self._evaluation()                  # Evaluate fitness
-            print("fitness:", self.pop[0].fitness)
+            # print("fitness:", self.pop[0].fitness)
 
     # Termination condition: 
     #   The termination condition has been decided to be a set number of iterations
